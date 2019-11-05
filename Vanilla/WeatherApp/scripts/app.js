28c7fe62b3c6c0946ab6cf40294844543deb845e
Animaniac <rdaislabie@gmail.com>
2019-11-05 18:00:07 +0000

    imgs for app

--- Vanilla/WeatherApp/scripts/app.js
+++ Vanilla/WeatherApp/scripts/app.js
@@ -1,16 +1,18 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('icon img');
+
 
 const updateUI = (data) => {
-    const cityInfo = data.cityInfo;
-    const cityWeather = data.weather;
+    const {cityInfo, weather} = data;
 
     details.innerHTML = `
         <h5 class="my-3">${cityInfo.EnglishName}</h5>
-        <div class="my-3">${cityWeather.WeatherText}</div>
+        <div class="my-3">${weather.WeatherText}</div>
         <div class="display-4 my-4">
-        <span>${cityWeather.Temperature.Metric.Value}</span>
+        <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
         </div>
     `;
