83cd2c5e8a0da4f4a1cecb45bc29afdad239a4ea
Animaniac <rdaislabie@gmail.com>
2019-11-05 17:42:18 +0000

    Ui updates from city details
    Just followed the tutorial along

--- Vanilla/WeatherApp/scripts/app.js
+++ Vanilla/WeatherApp/scripts/app.js
@@ -1,13 +1,26 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityInfo = data.cityInfo;
+    const cityWeather = data.weather;
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityInfo.EnglishName}</h5>
+        <div class="my-3">${cityWeather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${cityWeather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `;
+}
 
 const updateCity = async city => {
     const cityInfo = await getCity(city);
     const weather = await getWeather(cityInfo.Key);
     
-    return {
-        cityInfo: cityInfo,
-        weather: weather
-    };
+    return {cityInfo, weather};
 };
 
 cityForm.addEventListener('submit', e => {
@@ -18,6 +31,6 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     updateCity(city).then(data => {
-        console.log(data);
+        updateUI(data);
     }).catch(error => console.log(error));
 });
