ee47cbcf782dc8ab89d64cc7ac7341f8ac4643a3
Animaniac <rdaislabie@gmail.com>
2019-11-12 20:26:17 +0000

    delete and listners

--- Vanilla/FireBase/app.js
+++ Vanilla/FireBase/app.js
@@ -1,17 +1,65 @@
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
+const button = document.querySelector('button');
 
-const addRecipe = recipie => {
+
+const addRecipe = (recipie,id) => {
     let html = `
-        <li>
+        <li data-id ="${id}">
             <div>${recipie.title}</div>
             <div>${recipie.created_at.toDate()}</div>
-        </li>
+            <button class="btn btn-danger btn-sm my-2">Delete</button>
+        </li>        
     `;
     list.innerHTML += html;
 };
 
-db.collection('recipes').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        addRecipe(doc.data());
+const deleteRecipe = id => {
+    const recipies = document.querySelectorAll('li');
+
+    recipies.forEach(recipe =>{
+        if (recipe.getAttribute('data-id') === id){
+            recipe.remove();
+        }
     });
-}).catch(err => console.log(err));
+};
+
+const unsub = db.collection('recipes').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+
+        if(change.type === 'added'){
+            addRecipe(doc.data(), doc.id);
+        }else if(change.type = 'remove'){
+            deleteRecipe(doc.id);
+        }
+    });
+})
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+    }).catch(err => console.log(err));
+});
+
+list.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        }).catch(err => console.log(err));
+    }
+})
+
+button.addEventListener('click', () => {
+    unsub();
+    console.log('unsubbed');
+})
