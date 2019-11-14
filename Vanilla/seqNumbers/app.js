cb25c20c6a8ccb808cc6e59d4baf763715211623
Animaniac <rdaislabie@gmail.com>
2019-11-14 21:29:57 +0000

    Number Sequence problem
    Not working at the min....problem with the push

---
+++ Vanilla/seqNumbers/app.js
@@ -0,0 +1,22 @@
+//Given a list of integers L. Find the maximum length of sequential consecutive numbers that can be formed using L
+// [5,2,99,3,4,1,100]
+// problem from https://www.youtube.com/watch?v=XXLVi2y2GrY
+let numseq = [5,2,99,3,4,1,100];
+
+const findMaxSequence = (numbers =>{
+  let orderedNumbers = numbers.sort((a, b) => {return a-b});  
+  let numbersets = [];
+  let prev = 0;  
+
+  orderedNumbers.forEach(currnum => {
+    if(currnum - prev !== 1) numbersets.push([]);
+
+    numbersets[numbersets.length -1].push(currnum);
+
+    prev = currnum;
+  });
+
+  numbersets.sort((a,b) => b.length - a.length);
+
+  console.log(numbersets[0]);
+});
