5c46a8c74cb3a49f9dd063e0fdcb951387d405cd
Animaniac <rdaislabie@gmail.com>
2020-02-20 14:17:30 +0000

    Added comment sso you can follow how it works

--- Vanilla/Permutations/new.js
+++ Vanilla/Permutations/new.js
@@ -91,22 +91,26 @@ function solutionComments(input){
   document.clear();
   document.writeln("<pre>");
     const entries = Array.from(input.trim().replace(/\D/g,''));
-    let permutations = [];      
-    function permute(array, permutation = ""){
-      document.writeln("current permutation",permutation);
+    let permutations = [];
+    let itteration = 0
+    function permute(array, permutation = "", iteration){      
+      document.writeln("itteration ", iteration," current permutation ",permutation);
       if(array.length === 0){
         permutations.push(permutation);
+        itteration ++;
       }else{
         for(let i = 0; i < array.length; i++){
           let digitsToProcess = array.slice();
           document.writeln("digitsToProcess " + digitsToProcess);
           let firstDigit = digitsToProcess.splice(i,1); 
           document.writeln("firstDigit ", firstDigit);
-          document.writeln("permutations ", permutations);          
-          permute(digitsToProcess.slice(), permutation.concat(firstDigit));
+          document.writeln("permutations ", permutations);
+          document.writeln("permute: ", digitsToProcess.slice(),permutation.concat(firstDigit));          
+          permute(digitsToProcess.slice(), permutation.concat(firstDigit),itteration);
           }
       }
     }
+    document.writeln("start");
     permute(entries);
     console.log(permutations.toString());
   document.writeln("</pre>");
