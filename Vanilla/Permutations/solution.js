f12ede6f61c6a4468f295fa28204f2164f842dc3
Animaniac <rdaislabie@gmail.com>
2020-02-21 15:25:25 +0000

    remove some stuff

--- Vanilla/Permutations/solution.js
+++ Vanilla/Permutations/solution.js
@@ -1,50 +1,43 @@
-//Need to get an array that only contains ints from the input.
-//Then find all the permutations of the ints.
-//help from https://stackoverflow.com/questions/9960908/permutations-in-javascript
+// Need a program that will take each number & place it at index 0
+// Then each of the other digits at the different positions - depends on length
 
-const entryForm = document.querySelector('.entry');
-const results = document.querySelector('.results');
 
-const addPermutation = permutation =>{
-  const html =`
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${permutation}</span>
-  </li>`
-  results.innerHTML += html;
-};
-//recursive solution. Could potentially cause stackoverflow.
-const createPermutations = input => {
-  let permutations = [];
-  
-  const permute = (arr, permutation = "") =>{
-    if(arr.length === 0){
-      permutations.push(permutation);
-    }else{
-      for(let i = 0; i < arr.length; i++){
-        let currentDigit = arr.slice();
-        let nextDigit = currentDigit.splice(i,1);
-        permute(currentDigit.slice(), permutation.concat(nextDigit));
-      }
+// getPermutations (someString)
+// define permutations;
+// sanatise the input string and put it into an array
+// permute(array, permutationString)
+// if the array is empty then add the permutation to the results
+// loop through each number in the array
+// copy the array so we dont change the original
+// take the i letter & concatonate it to the permutation 
+// pas the spliced array and the new permutation into a new permute call.
+// first itteration would be i=0 permute([2,3],"1") - This then calls it again....but with i = 1-2etc
+//                             1 permute([3],"12")
+//                             2 permute([],"123")
+// 2nd                       i=1 0 permute([1,3],"2")
+//                               1 permute([3],"21")
+//                               2 permute([],"213")
+// 23rd                      i=2 0 permute([1,2],"3")
+//                               1 permute([2],"31")
+//                               2 permute([],"312")
+function solution(input){
+    const inputArray = [...input.trim().replace(/\D/g,'')];
+    let permutations = [];
+
+    const permute = (array, permutation = "") => {
+        if (array.length === 0) {
+            permutations.push(permutation);
+        }
+        else {
+            for (let i = 0; i < array.length; i++) {
+                let digitsToProcess = [...array];
+                let nextDigit = digitsToProcess.splice(i, 1);
+                permute([...digitsToProcess], permutation + nextDigit);
+            }
+        }
     }
-  }
-  permute(input);
-  results.innerHTML = "";
-  permutations.forEach(permutation =>{
-    addPermutation(permutation);
-  });
-}
+    permute(inputArray);
+    return permutations.toString();
+};
 
-entryForm.addEventListener('submit', e => {    
-  e.preventDefault();
 
-  const entries = Array.from(entryForm.entry.value.trim().replace(/\D/g,''));  
-  
-  if(entries.length){
-      console.log(createPermutations(entries));
-      entryForm.reset();
-  } else{
-    console.log(entry);
-    entry.ClassList.add("error");
-    //output some error or something on the inputform.
-  }   
-});
